10169b55cf403354e70de055564206fba643086c
Zack OConnor <zackary25@gmail.com>
2025-04-23 17:06:42 -0400

    CARMEN SANDIEGO - I found Carmen Sandiego

--- find_carmen.sql
+++ find_carmen.sql
@@ -37,17 +37,18 @@ SELECT countrycode, language FROM countrylanguage WHERE language = 'Italian' AND
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
-SELECT name FROM city WHERE countrycode = 'SMR' ORDER BY name ASC;
+SELECT name FROM city WHERE countrycode = 'VAT' ORDER BY name ASC;
 
 
 
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+SELECT name FROM city WHERE countrycode = 'SMR' ORDER BY name ASC;
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
-
+SELECT city.name FROM city JOIN country ON city.id = country.capital WHERE country.code = 'SMR';
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
@@ -63,5 +64,10 @@ SELECT name FROM city WHERE countrycode = 'SMR' ORDER BY name ASC;
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+SELECT name, countrycode FROM city WHERE population = 91084;
 
 
+SELECT * FROM city WHERE name LIKE '%�%' ORDER BY name ASC LIST 2;
+UPDATE city SET name = 'Brasília' WHERE name = 'Brasï¿½lia';
+UPDATE city SET name = 'A Coruña (La Coruña)' WHERE name = 'A Coru�a (La Coru�a)';
+UPDATE city SET name = 'Acapulco de Juárez' WHERE name = 'Acapulco de Ju�rez';
